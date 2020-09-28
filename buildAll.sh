c74d501f2396d8ddec5a1df494cfaec6823a230d
Antoine McTaggart <antoine.mctaggart@xero.com>
2020-09-28 11:07:57 -0400

    added scripts to install/build services

---
+++ buildAll.sh
@@ -0,0 +1,4 @@
+docker build -t kierkegaardsmiles/auth ./auth
+docker build -t kierkegaardsmiles/client ./client
+docker build -t kierkegaardsmiles/orders ./orders
+docker build -t kierkegaardsmiles/tickets ./tickets
---
+++ installAll.sh
@@ -0,0 +1,4 @@
+cd ./auth && npm i && cd ..
+cd ./client && npm i && cd ..
+cd ./orders && npm i && cd ..
+cd ./tickets && npm i && cd ..
